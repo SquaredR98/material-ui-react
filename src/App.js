b47cc22a2a11d06b221888875939f7bea108e0ca
Ravi Ranjan <yourfriendraviranjan@gmail.com>
2021-01-14 12:00:00 +0530

    First Commit with Header Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/ui/Header';
+import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import theme from './components/ui/Theme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<div className="App" color="secondary">
+					<Header />
+          <Switch>
+            <Route exact path="/" component={() => {
+              return <div>Hello</div>
+            }} />
+            <Route path="/services" component={() => {
+              return <div>Services</div>
+            }} />
+            <Route path="/customsoftware" component={() => {
+              return <div>customsoftware</div>
+            }} />
+            <Route path="/mobileapps" component={() => {
+              return <div>mobileapps</div>
+            }} />
+            <Route path="/websites" component={() => {
+              return <div>Hello</div>
+            }} />
+            <Route path="/revolution" component={() => {
+              return <div>The Revolution</div>
+            }} />
+            <Route path="/about" component={() => {
+              return <div>About</div>
+            }} />
+            <Route path="/contact" component={() => {
+              return <div>Contact Us</div>
+            }} />
+            <Route path="/estimate" component={() => {
+              return <div>Estimate</div>
+            }} />
+          </Switch>
+				</div>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
